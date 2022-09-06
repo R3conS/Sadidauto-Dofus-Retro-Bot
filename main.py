f2c7215752fe1849bf7a640c8287a141e71869ef
ephell <dremaitiserikas@yahoo.com>
2022-09-06 22:55:24 +0300

    Restructure
    - Move initialization of 'GameWindow' out of 'main()'.
    - Delete 'print()' statement.

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from gamewindow import GameWindow
 from bot import DofusBot, ImageData
 
 
+# Initializing the 'GameWindow()' object.
+gamewindow = GameWindow("Novella", "v1.35.1")
+
 # Initializing 'bot' object
 dofusbot = DofusBot(ImageData.gobbal_images_list, ImageData.gobbal_images_path, debug_window=True)
 #dofus_bot = DofusBot(ImageData.test_monster_images_list, ImageData.test_monster_images_path)
@@ -10,13 +13,9 @@ dofusbot = DofusBot(ImageData.gobbal_images_list, ImageData.gobbal_images_path, 
 
 def main():
 
-    # Initializing the 'GameWindow()' object.
-    gamewindow = GameWindow("Novella", "v1.35.1")
-
     if gamewindow.gamewindow_checkifexists() == True:
         gamewindow.gamewindow_resize()
         # Starting the 'bot' thread.
-        print(f"[INFO] Starting 'DofusBot_Thread' ... ")
         dofusbot.DofusBot_Thread_start()
     else:
         os._exit(1)
