94e70fd8204184d55b13dbe848995c2b6260f8e3
ephell <dremaitiserikas@yahoo.com>
2022-10-24 23:32:55 +0300

    Add command-line argument functionality.
    ***main.py***
    - Update 'bot' instance initialization logic by adding command-line parsing.
    
    ***bot.py***
    - Adjust constructor parameters.
    - Update docstring.
    
    ***cmd_line_parser.py***
    - Add 'cmd_line_parser.py' module.

---
+++ cmd_line_parser.py
@@ -0,0 +1,56 @@
+"""Provides command-line argument parsing functionality."""
+
+import argparse
+
+
+class Parser:
+    """Holds methods related to command-line argument parsing.
+    
+    Methods
+    ----------
+    parse_command_line()
+        Parse command-line arguments.
+
+    """
+
+    def __str_to_bool(self, argument):
+        """Convert 'str' argument values to 'bool'."""
+        if isinstance(argument, bool):
+            return argument
+        elif str(argument).lower() in ["true", "yes", "t", "y", "1"]:
+            return True
+        elif str(argument).lower() in ["false", "no", "f", "n", "0"]:
+            return False
+        else:
+            raise argparse.ArgumentTypeError("Boolean value expected.")
+
+    def parse_command_line(self):
+        """Parse command-line arguments."""
+        parser = argparse.ArgumentParser(description="Select 'Bot' options.")
+
+        parser.add_argument("-s", "--script",
+                            help="Name of bot script. "
+                                "Available: " 
+                                "'astrub_forest', "
+                                "'astrub_forest_reversed'.",
+                            required=True)
+
+        parser.add_argument("-cn", "--character_name", 
+                            help="Character's name.",
+                            required=True)
+
+        parser.add_argument("-ov", "--official_version", 
+                            help="Official or private 'Dofus Retro' server. "
+                                "Default = 'False'.",
+                            default=False,
+                            type=lambda x: self.__str_to_bool(x))  
+
+        parser.add_argument("-dw", "--debug_window",
+                            help="Launch visual debug window or not. "
+                                "Default = 'True'.",
+                            default=True,
+                            type=lambda x: self.__str_to_bool(x))
+
+        args = parser.parse_args()
+
+        return args
--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from bot import Bot
-
-
-# Initializing 'bot' object
-character_name = "Bobas"
-bot = Bot(script="astrub_forest",
-          character_name=character_name, 
-          official_version=False,
-          debug_window=True)
+from cmd_line_parser import Parser
 
 
 def main():
+
+    parser = Parser()
+    args = parser.parse_command_line()
+    bot = Bot(script=args.script,
+              character_name=args.character_name,
+              official_version=args.official_version,
+              debug_window=args.debug_window)
     bot.Bot_Thread_start()
 
 
