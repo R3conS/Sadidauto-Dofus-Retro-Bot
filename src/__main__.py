6a9c99797e97c09bf6a0a8fc17e53ba3d43226cd
ephell <dremaitiserikas@yahoo.com>
2023-11-22 13:06:17 +0200

    - Change exit hotkey to ctrl+alt+q. - Remove threading_tools.py. - Make Bot class inherit from Thread directly. - Remove docstrings. - Rename thread methods. - Call Initializing method in Bot constructor instead of main body. - Improve parameters from Initializing methods. Use cls variables directly.

--- src/__main__.py
+++ src/__main__.py
@@ -11,19 +11,18 @@ from cmd_line_parser import Parser
 
 def exit_with_hotkey():
     def exit():
-        log.info("Exit hotkey '<ctrl>+<alt>+w' pressed!")
-        log.info("Exiting ... ")
+        log.info("Exit hotkey '<ctrl>+<alt>+q' pressed! Exiting ... ")
         os._exit(1)
-    with keyboard.GlobalHotKeys({"<ctrl>+<alt>+w": exit}) as h:
+    with keyboard.GlobalHotKeys({"<ctrl>+<alt>+q": exit}) as h:
         h.join()
 
 
 def main():
-    args = Parser.parse_command_line()
-    bot = Bot(script=args.script, character_name=args.character_name)
-    bot.Bot_Thread_start()
-    # bot = Bot(script="af_anticlock", character_name="Juni")
-    # bot.Bot_Thread_start()
+    # args = Parser.parse_command_line()
+    # bot = Bot(script=args.script, character_name=args.character_name)
+    # bot.start()
+    bot = Bot(script="af_anticlock", character_name="Juni")
+    bot.start()
     exit_with_hotkey()
 
 
--- src/state/initializing.py
+++ src/state/initializing.py
@@ -5,7 +5,6 @@ import os
 
 import pygetwindow as gw
 
-from .botstate_enum import BotState
 import bank
 import combat as cbt
 import data
@@ -17,28 +16,27 @@ import window_capture as wc
 
 class Initializing:
 
-    script = None
-    character_name = None
-    official_version = None
-
+    __script = None
+    __character_name = None
     __window_suffixes = ["Dofus Retro", "Abrak"]
     __window_size = (950, 785)
     __window_pos = (-8, 0)
 
     @classmethod
-    def initializing(cls):
+    def initialize(cls, script: str, character_name: str):
         log.info("Starting initialization ... ")
+        cls.__script = script
+        cls.__character_name = character_name
         cls.__prepare_game_window()
-        cls.__load_bot_script_data(cls.script)
-        cls.__verify_character_name(cls.character_name)
+        cls.__load_bot_script_data()
+        cls.__verify_character_name()
         log.info("Initialization successful!")
-        return BotState.CONTROLLER
 
     @classmethod
     def __prepare_game_window(cls):
         log.info("Attempting to prepare Dofus window ... ")
-        if bool(gw.getWindowsWithTitle(cls.character_name)):
-            for w in gw.getWindowsWithTitle(cls.character_name):
+        if bool(gw.getWindowsWithTitle(cls.__character_name)):
+            for w in gw.getWindowsWithTitle(cls.__character_name):
                 if any(suffix in w.title for suffix in cls.__window_suffixes):
                     w.restore()
                     w.activate()
@@ -46,31 +44,31 @@ class Initializing:
                     w.moveTo(*cls.__window_pos)
                     log.info(f"Successfully prepared '{w.title}' Dofus window!")
                     return
-        log.critical(f"Failed to detect Dofus window for '{cls.character_name}'! Exiting ...")
+        log.critical(f"Failed to detect Dofus window for '{cls.__character_name}'! Exiting ...")
         os._exit(1)
 
     @classmethod
-    def __load_bot_script_data(cls, script):
+    def __load_bot_script_data(cls):
         log.info("Attempting to load bot script data ... ")
 
-        if not isinstance(script, str):
+        if not isinstance(cls.__script, str):
             log.critical("Parameter 'script' must be a string!")
             log.critical("Exiting ... ")
             os._exit(1)
 
-        script = script.lower()
-        if "af_" in script:
-            if script == "af_anticlock":
+        cls.__script = cls.__script.lower()
+        if "af_" in cls.__script:
+            if cls.__script == "af_anticlock":
                 hunting = data.scripts.astrub_forest.Hunting.Anticlock.data
-            elif script == "af_clockwise":
+            elif cls.__script == "af_clockwise":
                 hunting = data.scripts.astrub_forest.Hunting.Clockwise.data
-            elif script == "af_north":
+            elif cls.__script == "af_north":
                 hunting = data.scripts.astrub_forest.Hunting.North.data
-            elif script == "af_east":
+            elif cls.__script == "af_east":
                 hunting = data.scripts.astrub_forest.Hunting.East.data
-            elif script == "af_south":
+            elif cls.__script == "af_south":
                 hunting = data.scripts.astrub_forest.Hunting.South.data
-            elif script == "af_west":
+            elif cls.__script == "af_west":
                 hunting = data.scripts.astrub_forest.Hunting.West.data
 
             state.Controller.data_hunting = hunting
@@ -87,17 +85,17 @@ class Initializing:
 
             cbt.Combat.data_spell_cast = data.scripts.astrub_forest.Cast.data
             cbt.Combat.data_movement = data.scripts.astrub_forest.Movement.data
-            cbt.Combat.character_name = cls.character_name
+            cbt.Combat.character_name = cls.__character_name
 
             bank.Bank.img_path = data.images.npc.AstrubBanker.img_path
             bank.Bank.img_list = data.images.npc.AstrubBanker.img_list
             return
 
-        log.critical(f"Couldn't find script '{script}' in database! Exiting ... ")
+        log.critical(f"Couldn't find script '{cls.__script}' in database! Exiting ... ")
         os._exit(1)
 
     @classmethod
-    def __verify_character_name(cls, character_name):
+    def __verify_character_name(cls):
         log.info("Verifying character's name ... ")
         attempts_allowed = 3
         attempts_total = 0
@@ -106,7 +104,7 @@ class Initializing:
             if PopUp.interface("characteristics", "open"):
                 sc = wc.WindowCapture.custom_area_capture((685, 93, 205, 26))
                 r_and_t, _, _ = dtc.Detection.detect_text_from_image(sc)
-                if character_name == r_and_t[0][1]:
+                if cls.__character_name == r_and_t[0][1]:
                     PopUp.interface("characteristics", "close")
                     return True
                 else:
