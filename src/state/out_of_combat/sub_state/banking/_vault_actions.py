6c8bda63bf0b22b598c5eb131df10bf3a0c6cae7
ephell <dremaitiserikas@yahoo.com>
2023-11-28 12:07:32 +0200

    *_vault_actions.py: - Move PodsGetter to separate module. - Reorganize placement of methods in VaultActions class. - Add logger. - Move decorators to global scope. - Improve readability and logic flow in decorators. - Add verbose return strings to decorators. - Fix typo ing 'get_amount_of_occupied_slots()' method name.
    *_pods_getter.py:
    - Move PodsGetter to it's own module _pods_getter.py.
    - Reorganize method placement in the class.
    - Adjust arguments to variou OCR and cv2 calls in get_tooltip_rectangle_from_image().
    - Add docstring to read_text_from_tooltip().

--- src/state/out_of_combat/sub_state/banking/_vault_actions.py
+++ src/state/out_of_combat/sub_state/banking/_vault_actions.py
@@ -1,15 +1,95 @@
+from logger import Logger
+log = Logger.setup_logger("GLOBAL", Logger.DEBUG, True, True)
+
 from functools import wraps
 from time import perf_counter
 import os
 
 import cv2
-import numpy as np
 import pyautogui as pyag
-import win32api
 
+from ._pods_getter import PodsGetter
 from src.detection import Detection
 from src.window_capture import WindowCapture
-from src.ocr.ocr import OCR
+
+
+def _handle_tab_opening(decorated_method):
+    """Decorator."""
+    @wraps(decorated_method)
+    def wrapper(cls, *args, **kwargs):
+        tab_name = decorated_method.__name__.split("_")[1]
+        is_tab_open = getattr(VaultActions, f"is_{tab_name}_tab_open")
+        
+        if is_tab_open():
+            log.info(f"'{tab_name.capitalize()}' tab is open.")
+            return f"{tab_name}_tab_is_already_open"
+
+        tab_icon = Detection.find_image(
+            haystack=cls.get_inventory_tab_area_screenshot(),
+            needle=getattr(VaultActions, f"tab_{tab_name}_closed_image"),
+            confidence=0.95,
+            method=cv2.TM_CCOEFF_NORMED,
+        )
+        if len(tab_icon) > 0:
+            tab_x, tab_y = Detection.get_rectangle_center_point(tab_icon)
+            tab_x += cls.inventory_tab_area[0]
+            tab_y += cls.inventory_tab_area[1]
+
+            log.info(f"Opening '{tab_name.capitalize()}' tab ...")
+            pyag.moveTo(tab_x, tab_y)
+            pyag.click()
+
+            start_time = perf_counter()
+            while perf_counter() - start_time <= 5:
+                if is_tab_open():
+                    log.info(f"Successfully opened '{tab_name.capitalize()}' tab.")
+                    return f"successfully_opened_{tab_name}_tab"
+                
+            log.info(f"Failed to open '{tab_name.capitalize()}' tab.")
+            return f"failed_to_open_{tab_name}_tab"
+        
+        log.info(f"Failed to find '{tab_name.capitalize()}' tab icon.")
+        return f"failed_to_find_{tab_name}_tab_icon"
+    
+    return wrapper
+
+
+def _handle_tab_depositing(decorated_method):
+    """Decorator."""
+    @wraps(decorated_method)
+    def wrapper(cls, *args, **kwargs):
+        tab_name = decorated_method.__name__.split("_")[1]
+        open_tab = getattr(VaultActions, f"open_{tab_name}_tab")
+        is_tab_open = getattr(VaultActions, f"is_{tab_name}_tab_open")
+        open_tab()
+        if not is_tab_open():
+            log.info(f"Failed to open '{tab_name.capitalize()}' tab.")
+            return f"failed_to_open_{tab_name}_tab"
+
+        is_first_iteration = True
+        while True:
+            occupied_slots_amount = cls.get_amount_of_occupied_slots()
+            if occupied_slots_amount == 0:
+                if is_first_iteration:
+                    log.info(f"No items to deposit in '{tab_name.capitalize()}' tab.")
+                    return f"no_items_to_deposit_in_{tab_name}_tab"
+                else:
+                    log.info(f"Finished depositing items in '{tab_name.capitalize()}' tab.")
+                    return f"finished_depositing_items_in_{tab_name}_tab"
+
+            is_first_iteration = False
+            log.info(f"Depositing {occupied_slots_amount} items ...")
+            pods_before_deposit = PodsGetter.get_pods_numbers()[0]
+            cls.deposit_visible_items(occupied_slots_amount)
+            pods_after_deposit = PodsGetter.get_pods_numbers()[0]
+
+            if pods_after_deposit < pods_before_deposit:
+                log.info(f"Successfully deposited {occupied_slots_amount} items! Pods freed: {pods_before_deposit - pods_after_deposit}.")
+            else:
+                log.info(f"Failed to deposit items in {tab_name.capitalize()} tab.")
+                return f"failed_to_deposit_items_in_{tab_name}_tab"
+
+    return wrapper
 
 
 class VaultActions:
@@ -35,60 +115,34 @@ class VaultActions:
     inventory_tab_area = (684, 187, 234, 69)
 
     @classmethod
-    def deposit_items(cls, amount_of_items):
-        pyag.moveTo(687, 275) # To the left of the first slot.
-        pyag.click() # Click to make sure window is focused.
-        pyag.keyDown("ctrl")
-        pyag.keyDown("shift")
-        for coords in cls.get_deposit_coordinates(amount_of_items):
-            pyag.moveTo(coords[0], coords[1], duration=0.2) # Doesn't select items if moving faster
-        pyag.click(clicks=2, interval=0.1)
-        pyag.keyUp("shift")
-        pyag.keyUp("ctrl")
+    @_handle_tab_opening
+    def open_equipment_tab(cls):
+        pass
+    
+    @classmethod
+    @_handle_tab_opening
+    def open_misc_tab(cls):
+        pass
+    
+    @classmethod
+    @_handle_tab_opening
+    def open_resources_tab(cls):
+        pass
 
     @classmethod
-    def get_deposit_coordinates(cls, occupied_slots_amount):
-        slots_per_row = 5
-        slots_processed = 0
-        extracted_coords = []
-        for coords_list in cls.inventory_slot_coords.values():
-            slots_needed = occupied_slots_amount - slots_processed
-            if slots_needed >= slots_per_row:
-                extracted_coords.extend([coords_list[0], coords_list[-1]])
-                slots_processed += slots_per_row
-            elif slots_needed < slots_per_row and slots_needed > 0:
-                extracted_coords.extend([coords_list[0], coords_list[slots_needed - 1]])
-                slots_processed += slots_needed
-            if slots_processed >= occupied_slots_amount:
-                break
-        return extracted_coords
+    @_handle_tab_depositing
+    def deposit_equipment_tab(cls):
+        pass
 
     @classmethod
-    def get_amount_of_occuppied_slots(cls):
-        total_slots = 0
-        for _, slot_coords in cls.inventory_slot_coords.items():
-            for _ in slot_coords:
-                total_slots += 1
-        return total_slots - cls.get_amount_of_empty_slots()
+    @_handle_tab_depositing
+    def deposit_misc_tab(cls):
+        pass
 
     @classmethod
-    def get_amount_of_empty_slots(cls):
-        rectangles = Detection.find_image(
-            haystack=cls.get_inventory_slot_area_screenshot(),
-            needle=cls.empty_slot_image,
-            confidence=0.65,
-            method=cv2.TM_CCOEFF_NORMED,
-            get_best_match_only=False,
-        )
-        return len(cv2.groupRectangles(rectangles, 1, 0.5)[0])
-
-    @classmethod
-    def get_inventory_slot_area_screenshot(cls):
-        return WindowCapture.custom_area_capture(cls.inventory_slot_area)
-
-    @classmethod
-    def get_inventory_tab_area_screenshot(cls):
-        return WindowCapture.custom_area_capture(cls.inventory_tab_area)
+    @_handle_tab_depositing
+    def deposit_resources_tab(cls):
+        pass
 
     @classmethod
     def is_equipment_tab_open(cls):
@@ -123,125 +177,58 @@ class VaultActions:
             )
         ) > 0
 
-    def __handle_tab_opening(decorated_method):
-        """Decorator."""
-        @wraps(decorated_method)
-        def wrapper(cls, *args, **kwargs):
-            tab_name = decorated_method.__name__.split("_")[1]
-            is_open_method = getattr(VaultActions, f"is_{tab_name}_tab_open")
-            if not is_open_method():
-                tab_icon = Detection.find_image(
-                    haystack=cls.get_inventory_tab_area_screenshot(),
-                    needle=getattr(VaultActions, f"tab_{tab_name}_closed_image"),
-                    confidence=0.95,
-                    method=cv2.TM_CCOEFF_NORMED,
-                )
-                tab_x, tab_y = Detection.get_rectangle_center_point(tab_icon)
-                tab_x, tab_y = cls.inventory_tab_area[0] + tab_x, cls.inventory_tab_area[1] + tab_y
-                pyag.moveTo(tab_x, tab_y)
-                pyag.click()
-                start_time = perf_counter()
-                while perf_counter() - start_time <= 5:
-                    if is_open_method():
-                        return True
-                return False
-            return True
-        return wrapper
+    @classmethod
+    def get_deposit_coordinates(cls, occupied_slots_amount):
+        slots_per_row = 5
+        slots_processed = 0
+        extracted_coords = []
+        for coords_list in cls.inventory_slot_coords.values():
+            slots_needed = occupied_slots_amount - slots_processed
+            if slots_needed >= slots_per_row:
+                extracted_coords.extend([coords_list[0], coords_list[-1]])
+                slots_processed += slots_per_row
+            elif slots_needed < slots_per_row and slots_needed > 0:
+                extracted_coords.extend([coords_list[0], coords_list[slots_needed - 1]])
+                slots_processed += slots_needed
+            if slots_processed >= occupied_slots_amount:
+                break
+        return extracted_coords
 
     @classmethod
-    @__handle_tab_opening
-    def open_equipment_tab(cls):
-        pass
-    
+    def get_amount_of_occupied_slots(cls):
+        total_slots = 0
+        for _, slot_coords in cls.inventory_slot_coords.items():
+            for _ in slot_coords:
+                total_slots += 1
+        return total_slots - cls.get_amount_of_empty_slots()
+
     @classmethod
-    @__handle_tab_opening
-    def open_misc_tab(cls):
-        pass
-    
+    def get_amount_of_empty_slots(cls):
+        rectangles = Detection.find_image(
+            haystack=cls.get_inventory_slot_area_screenshot(),
+            needle=cls.empty_slot_image,
+            confidence=0.65,
+            method=cv2.TM_CCOEFF_NORMED,
+            get_best_match_only=False,
+        )
+        return len(cv2.groupRectangles(rectangles, 1, 0.5)[0])
+
     @classmethod
-    @__handle_tab_opening
-    def open_resources_tab(cls):
-        pass
+    def get_inventory_slot_area_screenshot(cls):
+        return WindowCapture.custom_area_capture(cls.inventory_slot_area)
 
+    @classmethod
+    def get_inventory_tab_area_screenshot(cls):
+        return WindowCapture.custom_area_capture(cls.inventory_tab_area)
 
-class PodsGetter:
-
-        @staticmethod
-        def screenshot_tooltip_area():
-            return WindowCapture.custom_area_capture((688, 527, 160, 30))
-
-        @staticmethod
-        def get_tooltip_rectangle_from_image(tooltip_area: np.ndarray):
-            tooltip_area = OCR.convert_to_grayscale(tooltip_area)
-            tooltip_area = cv2.morphologyEx(tooltip_area, cv2.MORPH_CLOSE, np.ones((4, 4), np.uint8))
-            tooltip_area = OCR.invert_image(tooltip_area)
-            tooltip_area = OCR.binarize_image(tooltip_area, 180)
-            canny = cv2.Canny(tooltip_area, 50, 150)
-            contours, _ = cv2.findContours(canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            for contour in contours:
-                epsilon = 0.05 * cv2.arcLength(contour, True)
-                approx = cv2.approxPolyDP(contour, epsilon, True)
-                x, y, w, h = cv2.boundingRect(approx)
-                if w > 50 and h > 18:
-                    return x, y, w, h
-            return None
-
-        @staticmethod
-        def crop_tooltip_from_image(screenshot: np.ndarray, rectangle: tuple[int, int, int, int]):
-            x, y, w, h = rectangle
-            return screenshot[y:y+h, x:x+w]
-
-        @staticmethod
-        def read_text_from_tooltip(tooltip: np.ndarray):
-            tooltip = OCR.convert_to_grayscale(tooltip)
-            tooltip = OCR.invert_image(tooltip)
-            tooltip = OCR.resize_image(tooltip, tooltip.shape[1] * 5, tooltip.shape[0] * 5)
-            tooltip = OCR.dilate_image(tooltip, 2)
-            tooltip = OCR.binarize_image(tooltip, 145)
-            tooltip = cv2.GaussianBlur(tooltip, (3, 3), 0)
-            return OCR.get_text_from_image(tooltip, ocr_engine="tesserocr")
-
-        @staticmethod
-        def trigger_pods_tooltip():
-            pyag.moveTo(735, 560)
-
-        @staticmethod
-        def hide_pods_tooltip():
-            pyag.moveTo(735, 580)
-
-        @classmethod
-        def is_pods_tooltip_visible(cls):
-            tooltip_area = cls.screenshot_tooltip_area()
-            tooltip_rectangle = cls.get_tooltip_rectangle_from_image(tooltip_area)
-            if tooltip_rectangle is not None:
-                return True
-            return False
-
-        @classmethod
-        def get_pods_numbers(cls) -> tuple[int, int]:
-            cls.trigger_pods_tooltip()
-            start_time = perf_counter()
-            while perf_counter() - start_time <= 5:
-                if cls.is_pods_tooltip_visible():
-                    tooltip_area = cls.screenshot_tooltip_area()
-                    tooltip_rectangle = cls.get_tooltip_rectangle_from_image(tooltip_area)
-                    cls.hide_pods_tooltip()
-                    if tooltip_rectangle is not None:
-                        tooltip = cls.crop_tooltip_from_image(tooltip_area, tooltip_rectangle)
-                        text = cls.read_text_from_tooltip(tooltip)
-                        return cls.parse_tooltip_text(text)
-            raise Exception(f"Timed out while getting pods amount.")
-        
-        @staticmethod
-        def parse_tooltip_text(extracted_tooltip_text) -> tuple[int, int]:
-            split_text = extracted_tooltip_text.split("pods out of")
-            numbers = []
-            for text in split_text:
-                text = text.strip()
-                text = text.replace(" ", "")
-                number = ""
-                for char in text:
-                    if char.isdigit():
-                        number += char
-                numbers.append(int(number))
-            return tuple(numbers)
+    @classmethod
+    def deposit_visible_items(cls, amount_of_items):
+        pyag.moveTo(687, 275) # To the left of the first slot.
+        pyag.click() # Click to make sure window is focused.
+        pyag.keyDown("ctrl")
+        pyag.keyDown("shift")
+        for coords in cls.get_deposit_coordinates(amount_of_items):
+            pyag.moveTo(coords[0], coords[1], duration=0.2) # Doesn't select items if moving faster
+        pyag.click(clicks=2, interval=0.1)
+        pyag.keyUp("shift")
+        pyag.keyUp("ctrl")
