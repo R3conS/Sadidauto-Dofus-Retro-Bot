868b5868b8b1aa1ad28bc15a23156c6c1c237b3c
ephell <dremaitiserikas@yahoo.com>
2023-11-28 09:09:26 +0200

    Add PodsGetter class to _vault_actions.py module

--- src/state/out_of_combat/sub_state/banking/_vault_actions.py
+++ src/state/out_of_combat/sub_state/banking/_vault_actions.py
@@ -32,7 +32,6 @@ class VaultActions:
     }
     inventory_slot_area = (687, 252, 227, 291)
     inventory_tab_area = (684, 187, 234, 69)
-    inventory_pod_tooltip_area = (688, 527, 160, 30)
 
     @classmethod
     def deposit_all_visible_items(cls):
@@ -128,11 +127,11 @@ class VaultActions:
         @wraps(decorated_method)
         def wrapper(cls, *args, **kwargs):
             tab_name = decorated_method.__name__.split("_")[1]
-            is_open_method = getattr(_VaultActions, f"is_{tab_name}_tab_open")
+            is_open_method = getattr(VaultActions, f"is_{tab_name}_tab_open")
             if not is_open_method():
                 tab_icon = Detection.find_image(
                     haystack=cls.get_inventory_tab_area_screenshot(),
-                    needle=getattr(_VaultActions, f"tab_{tab_name}_closed_image"),
+                    needle=getattr(VaultActions, f"tab_{tab_name}_closed_image"),
                     confidence=0.95,
                     method=cv2.TM_CCOEFF_NORMED,
                 )
@@ -163,36 +162,80 @@ class VaultActions:
     def open_resources_tab(cls):
         pass
 
-    @classmethod
-    def get_tooltip_area_image(cls):
-        return WindowCapture.custom_area_capture(cls.inventory_pod_tooltip_area)
 
-    @staticmethod
-    def get_tooltip_rectangle_from_image(tooltip_area_image: np.ndarray):
-        tooltip_area_image = OCR.convert_to_grayscale(tooltip_area_image)
-        tooltip_area_image = cv2.morphologyEx(tooltip_area_image, cv2.MORPH_CLOSE, np.ones((4, 4), np.uint8))
-        tooltip_area_image = OCR.invert_image(tooltip_area_image)
-        tooltip_area_image = OCR.binarize_image(tooltip_area_image, 180)
-        canny = cv2.Canny(tooltip_area_image, 50, 150)
-        contours, _ = cv2.findContours(canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        for contour in contours:
-            epsilon = 0.05 * cv2.arcLength(contour, True)
-            approx = cv2.approxPolyDP(contour, epsilon, True)
-            x, y, w, h = cv2.boundingRect(approx)
-            if w > 50 and h > 18:
-                return x, y, w, h
+class PodsGetter:
 
-    @staticmethod
-    def crop_tooltip_from_image(screenshot: np.ndarray, rectangle: tuple[int, int, int, int]):
-        x, y, w, h = rectangle
-        return screenshot[y:y+h, x:x+w]
+        @staticmethod
+        def screenshot_tooltip_area():
+            return WindowCapture.custom_area_capture((688, 527, 160, 30))
 
-    @staticmethod
-    def read_text_from_tooltip(tooltip: np.ndarray):
-        tooltip = OCR.convert_to_grayscale(tooltip)
-        tooltip = OCR.invert_image(tooltip)
-        tooltip = OCR.resize_image(tooltip, tooltip.shape[1] * 5, tooltip.shape[0] * 5)
-        tooltip = OCR.dilate_image(tooltip, 2)
-        tooltip = OCR.binarize_image(tooltip, 145)
-        tooltip = cv2.GaussianBlur(tooltip, (3, 3), 0)
-        return OCR.get_text_from_image(tooltip, ocr_engine="tesserocr")
+        @staticmethod
+        def get_tooltip_rectangle_from_image(tooltip_area: np.ndarray):
+            tooltip_area = OCR.convert_to_grayscale(tooltip_area)
+            tooltip_area = cv2.morphologyEx(tooltip_area, cv2.MORPH_CLOSE, np.ones((4, 4), np.uint8))
+            tooltip_area = OCR.invert_image(tooltip_area)
+            tooltip_area = OCR.binarize_image(tooltip_area, 180)
+            canny = cv2.Canny(tooltip_area, 50, 150)
+            contours, _ = cv2.findContours(canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            for contour in contours:
+                epsilon = 0.05 * cv2.arcLength(contour, True)
+                approx = cv2.approxPolyDP(contour, epsilon, True)
+                x, y, w, h = cv2.boundingRect(approx)
+                if w > 50 and h > 18:
+                    return x, y, w, h
+            return None
+
+        @staticmethod
+        def crop_tooltip_from_image(screenshot: np.ndarray, rectangle: tuple[int, int, int, int]):
+            x, y, w, h = rectangle
+            return screenshot[y:y+h, x:x+w]
+
+        @staticmethod
+        def read_text_from_tooltip(tooltip: np.ndarray):
+            tooltip = OCR.convert_to_grayscale(tooltip)
+            tooltip = OCR.invert_image(tooltip)
+            tooltip = OCR.resize_image(tooltip, tooltip.shape[1] * 5, tooltip.shape[0] * 5)
+            tooltip = OCR.dilate_image(tooltip, 2)
+            tooltip = OCR.binarize_image(tooltip, 145)
+            tooltip = cv2.GaussianBlur(tooltip, (3, 3), 0)
+            return OCR.get_text_from_image(tooltip, ocr_engine="tesserocr")
+
+        @staticmethod
+        def trigger_pods_tooltip():
+            pyag.moveTo(735, 560)
+
+        @classmethod
+        def is_pods_tooltip_visible(cls):
+            tooltip_area = cls.screenshot_tooltip_area()
+            tooltip_rectangle = cls.get_tooltip_rectangle_from_image(tooltip_area)
+            if tooltip_rectangle is not None:
+                return True
+            return False
+
+        @classmethod
+        def get_pods_numbers(cls) -> tuple[int, int]:
+            cls.trigger_pods_tooltip()
+            start_time = perf_counter()
+            while perf_counter() - start_time <= 5:
+                if cls.is_pods_tooltip_visible():
+                    tooltip_area = cls.screenshot_tooltip_area()
+                    tooltip_rectangle = cls.get_tooltip_rectangle_from_image(tooltip_area)
+                    if tooltip_rectangle is not None:
+                        tooltip = cls.crop_tooltip_from_image(tooltip_area, tooltip_rectangle)
+                        text = cls.read_text_from_tooltip(tooltip)
+                        return cls.parse_tooltip_text(text)
+            raise Exception(f"Timed out while getting pods amount.")
+        
+        @staticmethod
+        def parse_tooltip_text(extracted_tooltip_text) -> tuple[int, int]:
+            split_text = extracted_tooltip_text.split("pods out of")
+            numbers = []
+            for text in split_text:
+                text = text.strip()
+                text = text.replace(" ", "")
+                number = ""
+                for char in text:
+                    if char.isdigit():
+                        number += char
+                numbers.append(int(number))
+            return tuple(numbers)
