e5305943d0c0e37851fa871aa0fbcdbfef8313c7
ephell <dremaitiserikas@yahoo.com>
2023-12-03 14:00:14 +0200

    Add utilities.py

---
+++ src/utilities.py
@@ -0,0 +1,10 @@
+import os
+
+import cv2
+
+
+def load_image(image_folder_path: str, image_name: str):
+    image_path = os.path.join(image_folder_path, image_name)
+    if not os.path.exists(image_path) and not os.path.isfile(image_path):
+        raise FileNotFoundError(f"Image '{image_name}' not found in '{image_folder_path}'.")
+    return cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
