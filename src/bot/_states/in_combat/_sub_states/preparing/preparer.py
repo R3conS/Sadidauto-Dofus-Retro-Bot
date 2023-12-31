e70ec95dcbed06e719b7d71ac0b8245b072e4cfb
ephell <dremaitiserikas@yahoo.com>
2023-12-31 11:09:01 +0200

    Refactor preparer.py
    - Remove unused methods.
    - Add exceptions to control flow in most cases instead of Status codes.
    - Adjust variable names, comments, method names.

--- src/bot/_states/in_combat/_sub_states/preparing/preparer.py
+++ src/bot/_states/in_combat/_sub_states/preparing/preparer.py
@@ -16,6 +16,7 @@ from src.utilities import move_mouse_off_game_area, load_image, load_image_full_
 from src.bot._map_changer.map_changer import MapChanger
 from src.bot._states.in_combat._combat_options.combat_options import CombatOptions
 from src.bot._states.in_combat._status_enum import Status
+from src.bot._exceptions import RecoverableException
 
 
 class Preparer:
@@ -37,84 +38,53 @@ class Preparer:
         self._dummy_cell_data = MapDataGetter.get_data_object(script).get_dummy_cells()
 
     def prepare(self):
-        result = self._handle_fight_lock()
-        if (
-            result == Status.FAILED_TO_GET_FIGHT_LOCK_ICON_POS
-            or result == Status.TIMED_OUT_WHILE_TURNING_ON_FIGHT_LOCK
-        ):
-            return Status.FAILED_TO_FINISH_PREPARING
-        
-        result = self._handle_tactical_mode()
-        if (
-            result == Status.FAILED_TO_GET_TACTICAL_MODE_TOGGLE_ICON_POS
-            or result == Status.TIMED_OUT_WHILE_TURNING_ON_TACTICAL_MODE
-        ):
-            return Status.FAILED_TO_FINISH_PREPARING
+        while True:
+            try:
+                CombatOptions.FIGHT_LOCK.turn_on()
+                CombatOptions.TACTICAL_MODE.turn_on()
 
-        map_coords = MapChanger.get_current_map_coords()
-        
-        dummy_cell_color = None
-        result = self._handle_dummy_cells(map_coords)
-        if result == Status.FAILED_TO_MOVE_TO_DUMMY_CELLS:
-            return Status.FAILED_TO_FINISH_PREPARING
-        else:
-            if isinstance(result, str):
-                dummy_cell_color = result
-                # Prevent character's info tooltip from blocking starting cell locations from view.
-                move_mouse_off_game_area() 
-        
-        result = self._handle_starting_cells(map_coords, dummy_cell_color)
-        if result == Status.FAILED_TO_MOVE_TO_STARTING_CELLS:
-            return Status.FAILED_TO_FINISH_PREPARING
-        
-        result = self._handle_clicking_ready()
-        if (
-            result == Status.FAILED_TO_GET_READY_BUTTON_POS
-            or result == Status.TIMED_OUT_WHILE_CLICKING_READY_BUTTON
-        ):
-            return Status.FAILED_TO_FINISH_PREPARING
-        
-        result = self._has_combat_started()
-        if result == Status.TIMED_OUT_WHILE_DETECTING_START_OF_COMBAT:
-            return Status.FAILED_TO_FINISH_PREPARING
+                map_coords = MapChanger.get_current_map_coords()
+                if self._are_there_any_dummy_cells_on_map(map_coords):
+                    final_dummy_cell_color = self._select_dummy_cell(map_coords)
+                    # Mouse cursor will stay hovered over the character's model
+                    # which in turn will display the black character's info
+                    # tooltip (Name (lvl)) that blocks the view of the starting 
+                    # cell locations on some maps. The cursor needs to be moved 
+                    # off to make sure it doesn't happen.
+                    move_mouse_off_game_area()
+                    self._select_starting_cell(map_coords, final_dummy_cell_color)
+                else:
+                    self._select_starting_cell(map_coords)
+                
+                self._start_combat()
+            
+            except RecoverableException:
+                # ToDo: Call recovery code and try again until 3-4 times.
+                log.error("Recoverable exception occurred while banking. Exiting ...")
+                os._exit(1)
 
-        return Status.SUCCESSFULLY_FINISHED_PREPARING
+    def _are_there_any_dummy_cells_on_map(self, map_coords: str):
+        for map, cell_data in self._dummy_cell_data.items():
+            if map == map_coords:
+                if len(cell_data[self.RED]) > 0 or len(cell_data[self.BLUE]) > 0:
+                    return True
+        return False
 
-    @staticmethod
-    def _handle_fight_lock():
-        if CombatOptions.FIGHT_LOCK.is_on():
-            log.info("Fight lock is on.")
-            return Status.FIGHT_LOCK_IS_ALREADY_ON
-        log.info("Fight lock is off.")
-        return CombatOptions.FIGHT_LOCK.turn_on()
+    def _select_dummy_cell(self, map_coords: str):
+        log.info("Selecting a dummy cell ...")
 
-    @staticmethod
-    def _handle_tactical_mode():
-        if CombatOptions.TACTICAL_MODE.is_on():
-            log.info("Tactical mode is on.")
-            return Status.TACTICAL_MODE_IS_ALREADY_ON
-        log.info("Tactical mode is off.")
-        return CombatOptions.TACTICAL_MODE.turn_on()
-
-    def _handle_dummy_cells(self, map_coords: str):
-        log.info(f"Checking for dummy cells on map: {map_coords} ... ")
         red_dummy_cells = self._get_free_dummy_cells(self.RED, map_coords)
         blue_dummy_cells = self._get_free_dummy_cells(self.BLUE, map_coords)
-        if len(red_dummy_cells) == 0 and len(blue_dummy_cells) == 0:
-            log.info("No dummy cells found on this map.")
-            return Status.NO_DUMMY_CELLS_ON_THIS_MAP
-        
-        log.info("Dummy cells found. Attempting to move ... ")
 
         for cell_coords in red_dummy_cells:
             status = self._move_char_to_cell(*cell_coords)
             if status == Status.SUCCESSFULLY_MOVED_TO_CELL:
-                log.info(f"Successfully moved to red side dummy cell: {cell_coords}.")
+                log.info(f"Successfully selected red side dummy cell: {cell_coords}.")
                 return self.RED
         for cell_coords in blue_dummy_cells:
             status = self._move_char_to_cell(*cell_coords)
             if status == Status.SUCCESSFULLY_MOVED_TO_CELL:
-                log.info(f"Successfully moved to blue side dummy cell: {cell_coords}.")
+                log.info(f"Successfully selected blue side dummy cell: {cell_coords}.")
                 return self.BLUE
         
         if len(red_dummy_cells) == 0:
@@ -122,11 +92,10 @@ class Preparer:
         elif len(blue_dummy_cells) == 0:
             return self.BLUE
         
-        log.error("Failed to move to dummy cells.")
-        return Status.FAILED_TO_MOVE_TO_DUMMY_CELLS
+        raise RecoverableException("Failed to select a dummy cell.")
 
-    def _handle_starting_cells(self, map_coords: str, dummy_cell_color: str = None):
-        log.info(f"Attempting to move to a starting cell on map: {map_coords} ... ")
+    def _select_starting_cell(self, map_coords: str, dummy_cell_color: str = None):
+        log.info(f"Selecting a starting cell ...")
         if dummy_cell_color is not None:
             starting_cells = self._get_free_starting_cells(dummy_cell_color, map_coords)
         else:
@@ -135,40 +104,31 @@ class Preparer:
             starting_cells = red_starting_cells + blue_starting_cells
 
         for cell_coords in starting_cells:
-            result = self._move_char_to_cell(*cell_coords)
-            if result == Status.SUCCESSFULLY_MOVED_TO_CELL:
-                if dummy_cell_color is not None:
-                    log.info(f"Successfully moved to {dummy_cell_color} side starting cell: {cell_coords}.")
-                else:
-                    if cell_coords in red_starting_cells:
-                        log.info(f"Successfully moved to red side starting cell: {cell_coords}.")
-                    else:
-                        log.info(f"Successfully moved to blue side starting cell: {cell_coords}.")
-                return Status.SUCCESSFULLY_MOVED_TO_CELL
+            if self._move_char_to_cell(*cell_coords) == Status.SUCCESSFULLY_MOVED_TO_CELL:
+                log.info(f"Successfully selected starting cell: {cell_coords}.")
+                return
         
-        log.error("Failed to move to a starting cell.")
-        return Status.FAILED_TO_MOVE_TO_STARTING_CELLS
+        raise RecoverableException("Failed to select a starting cell.")
 
-    @classmethod
-    def _handle_clicking_ready(cls):
-        log.info("Clicking ready ... ")
-        ready_button_pos = cls._get_ready_button_pos()
+    def _start_combat(self):
+        log.info("Starting combat ... ")
+        ready_button_pos = self._get_ready_button_pos()
         if ready_button_pos is None:
-            log.error("Failed to get ready button position.")
-            return Status.FAILED_TO_GET_READY_BUTTON_POS
+            raise RecoverableException("Failed to get ready button position.")
         
         pyag.moveTo(*ready_button_pos)
         pyag.click()
         move_mouse_off_game_area()
 
+        timeout = 5
         start_time = perf_counter()
-        while perf_counter() - start_time <= 5:
-            if not cls._is_ready_button_visible():
-                log.info("Successfully clicked ready button.")
-                return Status.SUCCESSFULLY_CLICKED_READY_BUTTON
-        log.error("Timed out while clicking ready button.")
-        return Status.TIMED_OUT_WHILE_CLICKING_READY_BUTTON
+        while perf_counter() - start_time <= timeout:
+            if self._is_ap_counter_visible():
+                log.info("Successfully started combat.")
+                return
 
+        raise RecoverableException(f"Failed to detect the start of combat. Timed out: {timeout} seconds.")
+    
     def _move_char_to_cell(self, cell_x, cell_y):
         px_color_before_clicking_cell = pyag.pixel(cell_x, cell_y)
         self._click_cell(cell_x, cell_y)
@@ -199,7 +159,7 @@ class Preparer:
             if map == map_coords:
                 return cell_data[color]
         return []
-
+    
     def _get_free_starting_cells(self, map_coords: str, color: str):
         free_cells = []
         for cell_coords in self._get_starting_cells(color, map_coords):
@@ -207,6 +167,39 @@ class Preparer:
             if self._is_cell_free(*cell_coords, game_window_screenshot):
                 free_cells.append(cell_coords)
         return free_cells
+
+    def _did_char_move(self, cell_x, cell_y, px_color_before_moving: tuple):
+        start_time = perf_counter()
+        while perf_counter() - start_time <= 0.25:
+            if not pyag.pixelMatchesColor(cell_x, cell_y, px_color_before_moving):
+                return True
+        return False
+    
+    def _get_ready_button_pos(self):
+        rectangles = ImageDetection.find_images(
+            haystack=ScreenCapture.custom_area(self.READY_BUTTON_AREA),
+            needles=self.READY_BUTTON_IMAGES,
+            confidence=0.99,
+            method=cv2.TM_SQDIFF
+        )
+        if len(rectangles) > 0:
+            return ImageDetection.get_rectangle_center_point((
+                rectangles[0][0] + self.READY_BUTTON_AREA[0],
+                rectangles[0][1] + self.READY_BUTTON_AREA[1],
+                rectangles[0][2],
+                rectangles[0][3]
+            ))
+        return None
+    
+    def _is_ap_counter_visible(self):
+        return len(
+            ImageDetection.find_image(
+                haystack=ScreenCapture.custom_area(self.AP_COUNTER_AREA),
+                needle=self.AP_COUNTER_IMAGE,
+                confidence=0.99,
+                mask=self.AP_COUNTER_IMAGE_MASK
+            )
+        ) > 0
 
     @staticmethod
     def _is_cell_free(cell_x, cell_y, game_window_screenshot: Image.Image):
@@ -225,80 +218,6 @@ class Preparer:
         return False
 
     @staticmethod
-    def _are_images_same(before, after):
-        return len(
-            ImageDetection.find_image(
-                haystack=before,
-                needle=after,
-                method=cv2.TM_CCOEFF_NORMED,
-                confidence=0.99
-            )
-        ) > 0
-
-    @staticmethod
-    def _screenshot_cell_area(cell_x, cell_y):
-        return ScreenCapture.around_pos((cell_x, cell_y), 30)
-
-    @staticmethod
     def _click_cell(cell_x, cell_y):
         pyag.moveTo(cell_x, cell_y)
         pyag.click()
-
-    @classmethod
-    def _did_char_move(cls, cell_x, cell_y, px_color_before_moving: tuple):
-        start_time = perf_counter()
-        while perf_counter() - start_time <= 0.25:
-            if not pyag.pixelMatchesColor(cell_x, cell_y, px_color_before_moving):
-                return True
-        return False
-
-    @classmethod
-    def _is_ready_button_visible(cls):
-        if len(
-            ImageDetection.find_images(
-                haystack=ScreenCapture.custom_area(cls.READY_BUTTON_AREA),
-                needles=cls.READY_BUTTON_IMAGES,
-                confidence=0.98,
-                method=cv2.TM_SQDIFF_NORMED
-            )
-        ) > 0:
-            return True
-        return False
-
-    @classmethod
-    def _get_ready_button_pos(cls):
-        rectangles = ImageDetection.find_images(
-            haystack=ScreenCapture.custom_area(cls.READY_BUTTON_AREA),
-            needles=cls.READY_BUTTON_IMAGES,
-            confidence=0.99,
-            method=cv2.TM_SQDIFF
-        )
-        if len(rectangles) > 0:
-            return ImageDetection.get_rectangle_center_point((
-                rectangles[0][0] + cls.READY_BUTTON_AREA[0],
-                rectangles[0][1] + cls.READY_BUTTON_AREA[1],
-                rectangles[0][2],
-                rectangles[0][3]
-            ))
-        return None
-
-    @classmethod
-    def _is_ap_counter_visible(cls):
-        return len(
-            ImageDetection.find_image(
-                haystack=ScreenCapture.custom_area(cls.AP_COUNTER_AREA),
-                needle=cls.AP_COUNTER_IMAGE,
-                confidence=0.99,
-                mask=cls.AP_COUNTER_IMAGE_MASK
-            )
-        ) > 0
-    
-    @classmethod
-    def _has_combat_started(cls):
-        start_time = perf_counter()
-        while perf_counter() - start_time <= 5:
-            if cls._is_ap_counter_visible():
-                log.info("Successfully detected the start of combat.")
-                return Status.SUCCESSFULLY_DETECTED_START_OF_COMBAT
-        log.error("Timed out while detecting the start of combat.")
-        return Status.TIMED_OUT_WHILE_DETECTING_START_OF_COMBAT
