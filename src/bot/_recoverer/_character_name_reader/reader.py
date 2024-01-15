9678a2eb411ec2e06063273bfad2c174977bb95a
ephell <dremaitiserikas@yahoo.com>
2024-01-15 15:39:44 +0200

    Find character by full and partial names

--- src/bot/_recoverer/_character_name_reader/reader.py
+++ src/bot/_recoverer/_character_name_reader/reader.py
@@ -11,83 +11,130 @@ from src.image_detection import ImageDetection
 from src.screen_capture import ScreenCapture
 from src.ocr.ocr import OCR
 from src.bot._exceptions import UnrecoverableException
-from src.utilities import load_image_full_path
+from src.utilities import load_image_full_path, move_mouse_off_game_area
 
 
 class Reader:
 
     # Dofus client size must be 1000x785 for areas to be accurate.
-    NAME_AREAS = [
-        (81, 534, 190, 28), 
-        (250, 534, 190, 28),
-        (419, 534, 190, 28),
-        (589, 534, 190, 28),
-        (763, 534, 190, 28)
-    ]
-    NAME_TOOLTIP_TRIGGER_POS = [
-        (221, 559), (391, 559), (561, 559), (733, 559), (905, 559)
-    ]
+    CHAR_SLOT_INFO = {
+        1: {
+            "name_area": (81, 534, 190, 28), 
+            "level_area": (81, 561, 120, 25), 
+            "tooltip_trigger_pos": (221, 559),
+        },
+        2: {
+            "name_area": (250, 534, 190, 28), 
+            "level_area": (250, 561, 120, 25), 
+            "tooltip_trigger_pos": (391, 559)
+        },
+        3: {
+            "name_area": (420, 534, 190, 28), 
+            "level_area": (420, 561, 120, 25), 
+            "tooltip_trigger_pos": (561, 559)
+        },
+        4: {
+            "name_area": (589, 534, 190, 28), 
+            "level_area": (589, 561, 120, 25), 
+            "tooltip_trigger_pos": (733, 559)
+        },
+        5: {"name_area": (763, 534, 190, 28), 
+            "level_area": (763, 561, 120, 25), 
+            "tooltip_trigger_pos": (905, 559)
+        }
+    }
     NAME_TOOLTIP_DOTS_IMAGE = load_image_full_path(
         "src\\bot\\_recoverer\\_character_name_reader\\_images\\tooltip_dots.png"
     )
     FORBIDDEN_CHARACTERS = [
-        ".", ",", ":", ";", "!", "?", "|", "/", "\\", "(", ")", "{", "}", "<", ">"
+        ".", ",", ":", ";", "!", "?", "|", "/", "\\", "(", ")", "{", "}", "<", ">", " "
     ]
 
+    def __init__(self, character_name: str, character_level: int):
+        self._character_name = character_name
+        self._character_level = character_level
+
     @classmethod
-    def preprocess_screenshot_for_OCR(cls, sc: np.ndarray):
-        sc = OCR.resize_image(sc, sc.shape[1] * 10, sc.shape[0] * 10)
-        sc = OCR.convert_to_grayscale(sc)
-        sc = OCR.binarize_image(sc, 190)
-        sc = OCR.invert_image(sc)
-        sc = cv2.GaussianBlur(sc, (3, 3), 0)
-        return sc
-    
-    @classmethod
-    def read_name_area(cls, name_area, name_tooltip_trigger_pos):
-        if cls.are_tooltip_dots_visible(name_area):
-            cls.trigger_name_tooltip(name_area, name_tooltip_trigger_pos)
-        sc = ScreenCapture.custom_area(name_area)
-        sc = cls.preprocess_screenshot_for_OCR(sc)
+    def _read_name_area(cls, char_slot: int, read_tooltip: bool):
+        if read_tooltip and cls._are_name_tooltip_dots_visible(char_slot):
+            cls._trigger_name_tooltip(char_slot)
+            sc = ScreenCapture.custom_area(cls.CHAR_SLOT_INFO[char_slot]["name_area"])
+            move_mouse_off_game_area() # Untrigger tooltip.
+            sc = OCR.resize_image(sc, sc.shape[1] * 10, sc.shape[0] * 10)
+            sc = OCR.convert_to_grayscale(sc)
+            sc = OCR.binarize_image(sc, 190)
+            sc = OCR.invert_image(sc)
+            sc = cv2.GaussianBlur(sc, (3, 3), 0)
+        else:
+            sc = ScreenCapture.custom_area(cls.CHAR_SLOT_INFO[char_slot]["name_area"])
+            sc = OCR.convert_to_grayscale(sc)
+            sc = OCR.invert_image(sc)
+            sc = OCR.resize_image(sc, sc.shape[1] * 3, sc.shape[0] * 3)
+            sc = OCR.binarize_image(sc, 130)
+            sc = cv2.GaussianBlur(sc, (3, 3), 0)
+
         name = OCR.get_text_from_image(sc)
-        name = name[:20] # Max character name length is 20.
+        name = name[:21] # Max character name length is 20.
         name = "".join([c for c in name if c not in cls.FORBIDDEN_CHARACTERS])
         return name.strip()
 
-    def trigger_name_tooltip(name_area, name_tooltip_trigger_pos):
-        sc_before = ScreenCapture.custom_area(name_area)
+    @classmethod
+    def _trigger_name_tooltip(cls, char_slot: int):
+        sc_before = ScreenCapture.custom_area(cls.CHAR_SLOT_INFO[char_slot]["name_area"])
         timeout = 3
         start_time = perf_counter()
         while perf_counter() - start_time < timeout:
-            pyag.moveTo(*name_tooltip_trigger_pos)
-            sc_after = ScreenCapture.custom_area(name_area)
+            pyag.moveTo(*cls.CHAR_SLOT_INFO[char_slot]["tooltip_trigger_pos"])
+            sc_after = ScreenCapture.custom_area(cls.CHAR_SLOT_INFO[char_slot]["name_area"])
             if not np.array_equal(sc_before, sc_after):
                 return
             sleep(0.1)
         raise UnrecoverableException(f"Failed to trigger name tooltip in {timeout} seconds.")
 
-    @staticmethod
-    def read_all_character_names():
-        names = []
-        for name, trigger_pos in zip(Reader.NAME_AREAS, Reader.NAME_TOOLTIP_TRIGGER_POS):
-            names.append(Reader.read_name_area(name, trigger_pos))
-        return names
+    @classmethod
+    def _read_level_area(cls, char_slot: int):
+        area = ScreenCapture.custom_area(cls.CHAR_SLOT_INFO[char_slot]["level_area"])
+        area = OCR.convert_to_grayscale(area)
+        area = OCR.invert_image(area)
+        area = OCR.resize_image(area, area.shape[1] * 3, area.shape[0] * 3)
+        area = OCR.dilate_image(area, 2)
+        area = OCR.binarize_image(area, 110)
+        text = OCR.get_text_from_image(area)
+        if text == "":
+            return None
+        return int(text.split(" ")[-1].strip())
 
     @classmethod
-    def are_tooltip_dots_visible(cls, name_area):
+    def _are_name_tooltip_dots_visible(cls, char_slot: int):
         return len(
             ImageDetection.find_image(
-                haystack=ScreenCapture.custom_area(name_area),
+                haystack=ScreenCapture.custom_area(cls.CHAR_SLOT_INFO[char_slot]["name_area"]),
                 needle=cls.NAME_TOOLTIP_DOTS_IMAGE,
                 confidence=0.95
             )
         ) > 0
 
+    def _find_character_by_full_name(self):
+        log.info(f"Searching for character by full name ... ")
+        for char_slot in range(1, len(self.CHAR_SLOT_INFO) + 1):
+            if self._character_name == self._read_name_area(char_slot, read_tooltip=True):
+                log.info(f"Found character at slot: '{char_slot}'.")
+                return ImageDetection.get_rectangle_center_point(self.CHAR_SLOT_INFO[char_slot]["name_area"])
+        log.error(f"Failed to find character by full name.")
+        return None
+
+    def _find_character_by_partial_name(self):
+        log.info(f"Searching for character by partial name and level ... ")
+        for char_slot in range(1, len(self.CHAR_SLOT_INFO) + 1):
+            if (
+                self._character_name[:9] in self._read_name_area(char_slot, read_tooltip=False)
+                and self._character_level >= self._read_level_area(char_slot)
+            ):
+                log.info(f"Found character at slot: '{char_slot}'.")
+                return ImageDetection.get_rectangle_center_point(self.CHAR_SLOT_INFO[char_slot]["name_area"])
+        log.error(f"Failed to find character by partial name.")
+        return None
+
 
 if __name__ == "__main__":
-    # name = Reader.read_name_area(
-    #     Reader.NAME_AREAS[3], Reader.NAME_TOOLTIP_TRIGGER_POS[3]
-    # )
-    names = Reader.read_all_character_names()
-    print(names)
-    # print(Reader.are_tooltip_dots_visible(Reader.NAME_AREAS[4]))
+    reader = Reader("Qyjlgjgqjli-qyjgqyjg", 2)
