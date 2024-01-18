4b5b85fb4bedfa39b492113410bbd9b294fc0dc2
ephell <dremaitiserikas@yahoo.com>
2024-01-18 18:48:09 +0200

    - Add select_character(). - Rename class from Reader to Selector. - Rename dir to character_selector and module to selector.py. - Add name_area_with_tooltip, name_area_no_tooltip and slot_pos keys to CHAR_SLOT_INFO. - Add _wait_loading_screen_end(). - Add docstring to _find_character_by_partial_name().

--- src/bot/_recoverer/_character_selector/selector.py
+++ src/bot/_recoverer/_character_selector/selector.py
@@ -14,7 +14,7 @@ from src.bot._exceptions import UnrecoverableException
 from src.utilities import load_image_full_path, move_mouse_off_game_area
 
 
-class Reader:
+class Selector:
 
     # Dofus client size must be 1000x785 for areas to be accurate.
     # At certain client sizes some character's lower halves (like q, j, g etc.) 
@@ -24,28 +24,39 @@ class Reader:
     # adjust all the coords in the CHAR_SLOT_INFO dict accordingly.
     CHAR_SLOT_INFO = {
         1: {
-            "name_area": (81, 534, 190, 28), 
+            "name_area_with_tooltip": (81, 534, 190, 28), 
+            "name_area_no_tooltip": (81, 539, 145, 28),
             "level_area": (81, 561, 120, 25), 
             "tooltip_trigger_pos": (221, 559),
+            "slot_pos": (151, 470)
         },
         2: {
-            "name_area": (250, 534, 190, 28), 
+            "name_area_with_tooltip": (250, 534, 190, 28), 
+            "name_area_no_tooltip": (250, 539, 145, 28),
             "level_area": (250, 561, 120, 25), 
-            "tooltip_trigger_pos": (391, 559)
+            "tooltip_trigger_pos": (391, 559),
+            "slot_pos": (321, 470)
         },
         3: {
-            "name_area": (420, 534, 190, 28), 
+            "name_area_with_tooltip": (420, 534, 190, 28), 
+            "name_area_no_tooltip": (420, 539, 145, 28),
             "level_area": (420, 561, 120, 25), 
-            "tooltip_trigger_pos": (561, 559)
+            "tooltip_trigger_pos": (561, 559),
+            "slot_pos": (491, 470)
         },
         4: {
-            "name_area": (589, 534, 190, 28), 
+            "name_area_with_tooltip": (589, 534, 190, 28), 
+            "name_area_no_tooltip": (591, 539, 145, 28),
             "level_area": (589, 561, 120, 25), 
-            "tooltip_trigger_pos": (733, 559)
+            "tooltip_trigger_pos": (733, 559),
+            "slot_pos": (663, 470)
         },
-        5: {"name_area": (763, 534, 190, 28), 
+        5: {
+            "name_area_with_tooltip": (763, 534, 190, 28), 
+            "name_area_no_tooltip": (763, 539, 145, 28),
             "level_area": (763, 561, 120, 25), 
-            "tooltip_trigger_pos": (905, 559)
+            "tooltip_trigger_pos": (905, 559),
+            "slot_pos": (835, 470)
         }
     }
     NAME_TOOLTIP_DOTS_IMAGE = load_image_full_path(
@@ -59,16 +70,36 @@ class Reader:
         self._character_name = character_name
         self._character_level = character_level
 
+    def select_character(self):
+        log.info(f"Attempting to select character: '{self._character_name}' ... ")
+        slot_pos = self._find_character_by_full_name()
+        if slot_pos is None:
+            slot_pos = self._find_character_by_partial_name()
+        if slot_pos is None:
+            raise UnrecoverableException(f"Failed to select character.")
+        
+        log.info(f"Double clicking character slot ... ")
+        pyag.moveTo(*slot_pos)
+        pyag.click(clicks=2, interval=0.1)
+        self._wait_loading_screen_end()
+        log.info(f"Character selected successfully.")
+
     def _find_character_by_full_name(self):
         log.info(f"Searching for character by full name ... ")
         for char_slot in range(1, len(self.CHAR_SLOT_INFO) + 1):
             if self._character_name == self._read_name_area(char_slot, read_tooltip=True):
                 log.info(f"Found character at slot: '{char_slot}'.")
-                return ImageDetection.get_rectangle_center_point(self.CHAR_SLOT_INFO[char_slot]["name_area"])
+                return self.CHAR_SLOT_INFO[char_slot]["slot_pos"]
         log.error(f"Failed to find character by full name.")
         return None
 
     def _find_character_by_partial_name(self):
+        """
+        Finds the character by reading the name (without the tooltip) and
+        level areas. This is a fallback method in case the tooltip cannot be 
+        read properly, which happens quite often due to letters like 
+        'q', 'j', 'g'.
+        """
         log.info(f"Searching for character by partial name and level ... ")
         for char_slot in range(1, len(self.CHAR_SLOT_INFO) + 1):
             if (
@@ -76,7 +107,7 @@ class Reader:
                 and self._character_level >= self._read_level_area(char_slot)
             ):
                 log.info(f"Found character at slot: '{char_slot}'.")
-                return ImageDetection.get_rectangle_center_point(self.CHAR_SLOT_INFO[char_slot]["name_area"])
+                return self.CHAR_SLOT_INFO[char_slot]["slot_pos"]
         log.error(f"Failed to find character by partial name.")
         return None
 
@@ -84,7 +115,7 @@ class Reader:
     def _read_name_area(cls, char_slot: int, read_tooltip: bool):
         if read_tooltip and cls._are_name_tooltip_dots_visible(char_slot):
             cls._trigger_name_tooltip(char_slot)
-            sc = ScreenCapture.custom_area(cls.CHAR_SLOT_INFO[char_slot]["name_area"])
+            sc = ScreenCapture.custom_area(cls.CHAR_SLOT_INFO[char_slot]["name_area_with_tooltip"])
             move_mouse_off_game_area() # Untrigger tooltip.
             sc = OCR.resize_image(sc, sc.shape[1] * 10, sc.shape[0] * 10)
             sc = OCR.convert_to_grayscale(sc)
@@ -92,7 +123,7 @@ class Reader:
             sc = OCR.invert_image(sc)
             sc = cv2.GaussianBlur(sc, (3, 3), 0)
         else:
-            sc = ScreenCapture.custom_area(cls.CHAR_SLOT_INFO[char_slot]["name_area"])
+            sc = ScreenCapture.custom_area(cls.CHAR_SLOT_INFO[char_slot]["name_area_no_tooltip"])
             sc = OCR.convert_to_grayscale(sc)
             sc = OCR.invert_image(sc)
             sc = OCR.resize_image(sc, sc.shape[1] * 3, sc.shape[0] * 3)
@@ -119,12 +150,12 @@ class Reader:
 
     @classmethod
     def _trigger_name_tooltip(cls, char_slot: int):
-        sc_before = ScreenCapture.custom_area(cls.CHAR_SLOT_INFO[char_slot]["name_area"])
-        timeout = 3
+        sc_before = ScreenCapture.custom_area(cls.CHAR_SLOT_INFO[char_slot]["name_area_no_tooltip"])
+        timeout = 4
         start_time = perf_counter()
         while perf_counter() - start_time < timeout:
             pyag.moveTo(*cls.CHAR_SLOT_INFO[char_slot]["tooltip_trigger_pos"])
-            sc_after = ScreenCapture.custom_area(cls.CHAR_SLOT_INFO[char_slot]["name_area"])
+            sc_after = ScreenCapture.custom_area(cls.CHAR_SLOT_INFO[char_slot]["name_area_no_tooltip"])
             if not np.array_equal(sc_before, sc_after):
                 return
             sleep(0.1)
@@ -134,13 +165,28 @@ class Reader:
     def _are_name_tooltip_dots_visible(cls, char_slot: int):
         return len(
             ImageDetection.find_image(
-                haystack=ScreenCapture.custom_area(cls.CHAR_SLOT_INFO[char_slot]["name_area"]),
+                haystack=ScreenCapture.custom_area(cls.CHAR_SLOT_INFO[char_slot]["name_area_no_tooltip"]),
                 needle=cls.NAME_TOOLTIP_DOTS_IMAGE,
                 confidence=0.95
             )
         ) > 0
 
+    @staticmethod
+    def _wait_loading_screen_end():
+        log.info(f"Waiting for loading screen to end ... ")
+        timeout = 20
+        start_time = perf_counter()
+        while perf_counter() - start_time < timeout:
+            if pyag.pixelMatchesColor(973, 769, (213, 207, 170)):
+                log.info(f"Loading screen ended.")
+                return
+            sleep(0.1)
+        raise UnrecoverableException(
+            "Failed to detect end of loading screen. "
+            f"Timed out: {timeout} seconds."
+        )
+
 
 if __name__ == "__main__":
-    # reader = Reader("Qyjlgjgqjli-qyjgqyjg", 2)
-    reader = Reader("Blabeliblala", 2)
+    selector = Selector("Juni", 65)
+    selector.select_character()
