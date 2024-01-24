0880d0d8f0f84fb9feaea217a5eb351d5950c351
ephell <dremaitiserikas@yahoo.com>
2024-01-24 23:18:22 +0200

    Add logic that checks if map and minimap has loaded after logging in

--- src/bot/_recoverer/recoverer.py
+++ src/bot/_recoverer/recoverer.py
@@ -3,6 +3,7 @@ log = Logger.setup_logger("GLOBAL", Logger.DEBUG, True, True)
 
 from time import perf_counter, sleep
 
+import numpy as np
 import pyautogui as pyag
 import pygetwindow as gw
 
@@ -41,18 +42,11 @@ class Recoverer:
         self._character_selector = CharacterSelector(character_name, character_level)
 
     def recover(self):
-        if not self._is_character_logged_in():
+        if not self._is_control_area_visible():
             self._login()
             return self._determine_bot_state()
         Interfaces.close_all()
         return self._determine_bot_state()
-
-    def _is_character_logged_in(self):
-        return (
-            pyag.pixelMatchesColor(673, 747, (213, 207, 170))
-            # Color is different when an offer (exchange, group invite, etc.) is on screen.
-            or pyag.pixelMatchesColor(673, 747, (192, 186, 153))
-        )
 
     def _login(self):
         log.info("Attempting to log in ... ")
@@ -69,23 +63,30 @@ class Recoverer:
                 "interface is open nor is the 'Log in' button visible."
             )
 
-        self._wait_loading_screen_end()
+        self._wait_for_server_selection_screen()
         self._resize_game_window(self.WINDOW_SIZE_FOR_SERVER_AND_CHAR_SELECTION)
+        
         self._server_selector.select_server()
         # Character selection screen is skipped if character is in combat.
-        if self._is_character_logged_in():
+        if self._is_control_area_visible():
+            self._resize_game_window(self._game_window_default_size)
+            self._wait_for_map_and_minimap_to_load()
             log.info(f"Succesfully logged in")
-            self._resize_game_window(self._game_window_default_size)
             return
+        
         self._character_selector.select_character()
-        if self._is_character_logged_in():
-            log.info(f"Succesfully logged in.")
+        if self._is_control_area_visible():
             self._resize_game_window(self._game_window_default_size)
+            self._wait_for_map_and_minimap_to_load()
+            log.info(f"Succesfully logged in")
             return
 
     def _resize_game_window(self, size: tuple[int, int]):
         log.info(f"Resizing game window to: {size}.")
         self._game_window.resizeTo(*size)
+        # Putting the window back to most top left corner. After resizing
+        # it's for some reason slightly moved off the left side of the screen.
+        self._game_window.moveTo(-8, 0)
 
     @classmethod
     def _determine_bot_state(cls):
@@ -113,17 +114,27 @@ class Recoverer:
             raise UnrecoverableException("Invalid 'game_window_identifier' type!")
 
     @classmethod
-    def _wait_loading_screen_end(cls):
-        log.info(f"Waiting for the loading screen to end ... ")
+    def _get_login_button_pos(cls):
+        return ImageDetection.get_rectangle_center_point(
+            ImageDetection.find_image(
+                haystack=ScreenCapture.game_window(),
+                needle=cls.LOGIN_BUTTON_IMAGE,
+                confidence=0.95,
+            )
+        )
+
+    @classmethod
+    def _wait_for_server_selection_screen(cls):
+        log.info(f"Waiting for the server selection screen to load ... ")
         timeout = 30
         start_time = perf_counter()
         while perf_counter() - start_time < timeout:
             if cls._is_server_selection_screen_visible():
-                log.info(f"Loading screen has ended.")
+                log.info(f"Server selection screen has loaded.")
                 return
-            sleep(0.1)
+            sleep(0.25)
         raise UnrecoverableException(
-            "Failed to detect end of the loading screen. "
+            "Failed to detect server selection screen. "
             f"Timed out: {timeout} seconds."
         )
 
@@ -142,18 +153,62 @@ class Recoverer:
                 confidence=0.95,
             )
         ) > 0
-    
+
+    @staticmethod
+    def _is_control_area_visible():
+        """Chat, minimap, interface icons, spell/item bar etc."""
+        return (
+            pyag.pixelMatchesColor(673, 747, (213, 207, 170))
+            # Color is different when an offer (exchange, group invite, etc.) is on screen.
+            or pyag.pixelMatchesColor(673, 747, (192, 186, 153))
+        )
+
+    @staticmethod
+    def _has_map_loaded():
+        """
+        Map is the game area where the character can move around. It's 
+        considered loaded when there is at least one non-black pixel in
+        the search areas.
+        """
+        sc = ScreenCapture.game_window()
+        search_areas = [
+            sc[90:170, 5:900], # (x5, y90, w900, h170)
+            sc[450:530, 5:900], # (x5, y450, w900, h170)
+            sc[90:500, 5:150] # (x5, y90, w150, h500)
+        ]
+        for area in search_areas:
+            if np.any(area != 0):
+                return True
+        return False
+
+    @staticmethod
+    def _has_minimap_loaded():
+        sc = ScreenCapture.game_window()
+        pixels = [ # sc[y, x]
+            sc[667, 501],
+            sc[667, 548],
+            sc[692, 501],
+            sc[692, 548]
+        ]
+        return any(np.all(pixel[::-1] != (242, 144, 110)) for pixel in pixels)
+
     @classmethod
-    def _get_login_button_pos(cls):
-        return ImageDetection.get_rectangle_center_point(
-            ImageDetection.find_image(
-                haystack=ScreenCapture.game_window(),
-                needle=cls.LOGIN_BUTTON_IMAGE,
-                confidence=0.95,
-            )
+    def _wait_for_map_and_minimap_to_load(cls):
+        log.info("Waiting for the map and minimap to load ... ")
+        timeout = 30
+        start_time = perf_counter()
+        while perf_counter() - start_time < timeout:
+            if cls._has_map_loaded() and cls._has_minimap_loaded():
+                log.info("Map and minimap have loaded.")
+                return
+            sleep(0.25)
+        raise UnrecoverableException(
+            "Failed to detect if map and minimap have loaded. "
+            f"Timed out: {timeout} seconds."
         )
 
 
 if __name__ == "__main__":
+    # recoverer = Recoverer("Kofas", "Boune", 65, "Dofus Retro", (950, 785))
     recoverer = Recoverer("Juni", "Semi-like", 65, "Abrak", (950, 785))
     recoverer._login()
