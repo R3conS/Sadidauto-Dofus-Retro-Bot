4f92c7040e3fb9b1e16c0575e38c95607d694b6a
ephell <dremaitiserikas@yahoo.com>
2024-01-25 05:18:17 +0200

    Adjust image folder path

--- tests/test_map_changer/test_map_changer.py
+++ tests/test_map_changer/test_map_changer.py
@@ -6,18 +6,18 @@ from src.bot._map_changer._map_data import DATA as MAP_DATA
 
 class TestMapChanger(unittest.TestCase):
 
-    _IMAGE_FOLDER_FOLDER_PATH = "src\\bot\\_map_changer\\_images"
+    _IMAGE_FOLDER_PATH = "src\\bot\\_map_changer\\_images"
 
     def test_map_data(self):
         """Make sure that all map coords in map data have a corresponding image file."""
         image_names = []
-        for file_name in [name for name in os.listdir(self._IMAGE_FOLDER_FOLDER_PATH) if name.endswith(".png")]:
+        for file_name in [name for name in os.listdir(self._IMAGE_FOLDER_PATH) if name.endswith(".png")]:
             image_names.append(file_name.replace(".png", ""))
         for map_coords in MAP_DATA.keys():
             self._custom_assertIn(
                 map_coords, 
                 image_names,
-                msg=f"Image file for map coords '{map_coords}' not found in folder: '{self._IMAGE_FOLDER_FOLDER_PATH}'."
+                msg=f"Image file for map coords '{map_coords}' not found in folder: '{self._IMAGE_FOLDER_PATH}'."
             )
 
     def _custom_assertIn(self, member, container, msg):
