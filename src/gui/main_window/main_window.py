36a7ce0fd75e354500d18d0308bc52176a0d5ddc
ephell <dremaitiserikas@yahoo.com>
2024-02-06 17:32:05 +0200

    Save 'app' on self

--- src/gui/main_window/main_window.py
+++ src/gui/main_window/main_window.py
@@ -8,13 +8,12 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
     def __init__(self, app: QApplication):
         super().__init__()
+        self.app = app
         self.setupUi(self)
         self.setWindowTitle("oFlexBot")
-        self.app = app
         self.setFocus()
         self.setFocusPolicy(Qt.ClickFocus) # Make all widgets lose focus when clicking on the main window.
         self.setWindowFlags(Qt.Dialog | Qt.MSWindowsFixedSizeDialogHint) # Set non resizable.
-        self.character_name_line_edit.setText("Chick-[AYU]")
         self._connect_signals_and_slots()
 
     def _connect_signals_and_slots(self):
