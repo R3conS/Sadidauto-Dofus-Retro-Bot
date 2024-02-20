1f837f5ff45abc517d556272aca9320782163efb
ephell <dremaitiserikas@yahoo.com>
2024-02-20 23:59:58 +0200

    Update status label with error message when initialization options are invalid

--- src/gui/main_window/button_start.py
+++ src/gui/main_window/button_start.py
@@ -5,7 +5,7 @@ log = get_logger()
 from time import perf_counter, time
 
 from PySide6.QtCore import QThread, Signal
-from PySide6.QtWidgets import QPushButton
+from PySide6.QtWidgets import QPushButton, QMainWindow
 
 from src.bot.bot import Bot
 
@@ -14,13 +14,40 @@ class StartButton(QPushButton):
 
     bot_started_signal = Signal(Bot, float) # Bot, start_time
     bot_exited_due_to_exception_signal = Signal()
+    initialization_options_invalid_signal = Signal(str)
 
     def __init__(self, parent=None):
         super().__init__(parent)
         self.clicked.connect(self._on_button_clicked)
 
+    def on_bot_stopped(self):
+        self._label_updater.stopped = True
+        self.setEnabled(True)
+
+    def on_bot_exited_due_to_exception(self):
+        self.bot_exited_due_to_exception_signal.emit()
+        self.setEnabled(True)
+        self.window().setFocus() # Without this the focus for some reason shifts to character name line edit
+
     def _on_button_clicked(self):
         main_window = self.window()
+        if not self._are_initialization_options_valid(main_window):
+            return
+        self._start_bot_process(main_window)
+
+    def _are_initialization_options_valid(self, main_window: QMainWindow):
+        if main_window.character_name_line_edit.text() == "":
+            self.initialization_options_invalid_signal.emit("Character name is not set!")
+            return False
+        if main_window.server_combo_box.currentIndex() == -1:
+            self.initialization_options_invalid_signal.emit("Server is not selected!")
+            return False
+        if main_window.script_combo_box.currentIndex() == -1:
+            self.initialization_options_invalid_signal.emit("Script is not selected!")
+            return False
+        return True
+
+    def _start_bot_process(self, main_window: QMainWindow):
         bot = Bot(
             script=main_window.script_combo_box.currentText(),
             character_name=main_window.character_name_line_edit.text(),
@@ -36,15 +63,6 @@ class StartButton(QPushButton):
         self._label_updater.bot_exited_due_to_exception.connect(self.on_bot_exited_due_to_exception)
         self._label_updater.start()
         self.setEnabled(False)
-
-    def on_bot_stopped(self):
-        self._label_updater.stopped = True
-        self.setEnabled(True)
-
-    def on_bot_exited_due_to_exception(self):
-        self.bot_exited_due_to_exception_signal.emit()
-        self.setEnabled(True)
-        self.window().setFocus() # Without this the focus for some reason shifts to character name line edit
 
 
 class _RunTimeDurationLabelUpdater(QThread):
--- src/gui/main_window/label_status.py
+++ src/gui/main_window/label_status.py
@@ -5,4 +5,8 @@ class StatusLabel(QLabel):
 
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.setStyleSheet("color: yellow")
+
+    def on_initialization_options_invalid(self, message: str):
         self.setStyleSheet("color: red")
+        self.setText(message)
