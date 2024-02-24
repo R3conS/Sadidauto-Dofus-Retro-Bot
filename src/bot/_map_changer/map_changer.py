9bf7b85fde3c447977b54f62b92662dda25b0a2d
ephell <dremaitiserikas@yahoo.com>
2024-02-25 00:28:10 +0200

    Inrease loading screen wait time to 15 seconds

--- src/bot/_map_changer/map_changer.py
+++ src/bot/_map_changer/map_changer.py
@@ -72,7 +72,7 @@ class MapChanger:
     def wait_loading_screen_pass():
         log.info("Waiting for loading screen ... ")
         start_time = perf_counter()
-        while perf_counter() - start_time <= 10:
+        while perf_counter() - start_time <= 15:
             if MapChanger._is_loading_screen_visible():
                 log.info("Loading screen detected ... ")
                 break
@@ -83,7 +83,7 @@ class MapChanger:
             )
         
         start_time = perf_counter()
-        while perf_counter() - start_time <= 10:
+        while perf_counter() - start_time <= 15:
             if not MapChanger._is_loading_screen_visible():
                 log.info("Loading screen finished.")
                 return
