4fe8a4f177a65601b1b17e25351cd9e08e0b009f
ephell <dremaitiserikas@yahoo.com>
2024-02-25 00:27:05 +0200

    Reduce max pods percentage to 90%

--- src/gui/main_window/spin_box_pods_percentage.py
+++ src/gui/main_window/spin_box_pods_percentage.py
@@ -6,8 +6,8 @@ class PodsPercentageSpinBox(QSpinBox):
 
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.setRange(0, 95)
-        self.setValue(95)
+        self.setRange(0, 90)
+        self.setValue(90)
         self.setSuffix("%")
         self.setSingleStep(1)
         self.setAlignment(Qt.AlignCenter)
