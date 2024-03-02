7d95025c3ecea47e3f1dd9cfd1ddf6a30c874753
ephell <dremaitiserikas@yahoo.com>
2024-03-02 04:53:49 +0200

    Remove pynput dependency

--- src/__main__.py
+++ src/__main__.py
@@ -2,25 +2,13 @@ from src.logger import get_logger
 
 log = get_logger()
 
-import os
 import sys
-
-from pynput import keyboard
 
 from src.gui.application.application import Application
 from src.gui.main_window.main_window import MainWindow
 
 
-def exit_with_hotkey():
-    def exit():
-        log.info("Exit hotkey '<ctrl>+<alt>+w' pressed! Exiting ... ")
-        os._exit(0)
-    with keyboard.GlobalHotKeys({"<ctrl>+<alt>+w": exit}) as h:
-        h.join()
-
-
 def main():
-    # ToDo: add ability to run bot headless without GUI.
     app = Application(sys.argv)
     main_window = MainWindow(app)
     main_window.show()
